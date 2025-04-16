8fc5d68bb03d9b73b4d9e206509f7704af40ae19
quihost <quihost@gmail.com>
2025-04-16 18:01:07 +0200

    Fix: Corretto import del componente Dashboard

--- app/cleaner/dashboard/page.tsx
+++ app/cleaner/dashboard/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
-import { Dashboard } from '@/components/Dashboard'
+import { Dashboard } from '@/app/components/Dashboard'
 
 export default function CleanerDashboardPage() {
   const { data: session } = useSession()
 
   return (
-    <Dashboard userType="cleaner" user={session?.user} />
+    <Dashboard userType="cleaner" />
   )
 } 
